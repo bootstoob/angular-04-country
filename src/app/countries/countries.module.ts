7442cab4260593c6f00a7fa323e0f6f5079b2f35
Sergio Mazón Aracil <smazonar@emeal.nttdata.com>
2023-12-29 11:01:38 +0100

    sidebar, más módulos y lazyLoad

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
+import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { CountiresRoutingModule } from './countries-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ByCapitalPageComponent,
+    ByRegionPageComponent,
+    CountryPageComponent,
+    ByCountryPageComponent,
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    CountiresRoutingModule,
   ]
 })
 export class CountriesModule { }
