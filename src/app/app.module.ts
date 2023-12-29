2e07d1c3354b364f97b6a6e6d3747bd8f466bbb0
Sergio Mazón Aracil <smazonar@emeal.nttdata.com>
2023-12-29 09:23:32 +0100

    AppRoutingModule & router-outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    AppRoutingModule,
+
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
