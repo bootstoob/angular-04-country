09a5ff4f93abb78f02b860c665cd7e7db839ae93
Sergio Mazón Aracil <smazonar@emeal.nttdata.com>
2024-01-01 20:57:46 +0100

    search-by-: todos

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, pipe, tap, of } from 'rxjs';
 
 import { Country } from '../interfaces/country';
 
@@ -16,10 +16,33 @@ export class CountriesService {
         //return this.http.get<Country[]>(`${this.apiUrl}/capital/san`)
         //ó esto:
         const url = `${ this.apiUrl }/capital/${ term }`;
-        return this.http.get<Country[]>( url );
-        //  .pipe(
-        //    catchError( () => of([]) )
-        //);
+        return this.http.get<Country[]>( url )
+          .pipe(
+            //1
+            //tap(countries => console.log('Pasop por el TAP', countries))
+            //2
+            /*catchError(error=>{
+                console.log(error);
+                return of([])
+            })*/
+            //3
+            catchError( () => of([]) )
+        );
     }
-
+    searchCountry( term: string ): Observable<Country[]> {
+        const url = `${ this.apiUrl }/name/${ term }`;
+        return this.http.get<Country[]>( url )
+        .pipe(
+            catchError( () => of([]) )
+        );
+    }
+    
+    searchRegion( region: string ): Observable<Country[]> {
+        const url = `${ this.apiUrl }/region/${ region }`;
+        return this.http.get<Country[]>( url )
+        .pipe(
+            catchError( () => of([]) )
+        );
+    }
+    
 }
